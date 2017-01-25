5f3cfd4249001c142434e3200c46d78ed83bf228
Maikel Dollé <maikel@itmagix.nl>
2017-01-25 19:47:59 +0100

    Removed garbage

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t rabo-fitnesse .
-docker run -ti -p 80:8080 -v ~/fitnesse:/opt/fitnesse/FitNesseRoot rabo-fitnesse
--- cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for i in `docker ps -a -q` ; do docker stop $i ; docker rm $i ; done
-for i in `docker images -q` ; do docker rmi $i ; done
